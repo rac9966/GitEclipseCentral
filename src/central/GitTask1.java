c0b35babff3ffb9fbe83787c156f3d5a4b244040
rac9966 <rachananaidum@gmail.com>
2025-04-23 21:02:31 +0530

    pushing the project to central

---
+++ src/central/GitTask1.java
@@ -0,0 +1,19 @@
+package central;
+
+public class GitTask1 
+{
+	static int a = 10;
+	static int b = 20;
+	static int c;
+	
+	
+	public static void main(String[] args)
+	{
+		c = a;
+		a = b;
+		b = c;
+		
+		
+	}
+ 
+}
