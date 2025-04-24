4df4c808b0261d553b284f82fe0893ad7bb7b7d6
rac9966 <rachananaidum@gmail.com>
2025-04-24 20:51:10 +0530

    GitTask4 is added

--- src/central/Task3.java
+++ src/central/Task3.java
@@ -1,6 +1,7 @@
+package central;
 public class Task3
 {
-  String s1 = "Git"
+  static String s1 = "Git";
 public static void main(String[] args)
   {
   System.out.println(s1);
---
+++ src/subbranch1/GitTask4.java
@@ -0,0 +1,12 @@
+package subbranch1;
+
+public class GitTask4 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello!");
+
+	}
+
+}
